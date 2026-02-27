9c92decd8de6d6556693266932d5bec541e020c5
AFKMartin <javiermarace@protonmail.com>
2026-02-27 20:31:47 +0000

    layout modifications

--- layout/layout.py
+++ layout/layout.py
@@ -10,51 +10,66 @@ class Layout:
         self.width = width
         self.rtl = rtl
         self.font = font
+        self.size = 12
 
         if self.rtl:
-            cursor_x = self.width - HSTEP
-            cursor_y = VSTEP
+            self.cursor_x = self.width - HSTEP
+            self.cursor_y = VSTEP
             for tok in tokens:
                 if isinstance(tok, Text):
                     continue
                 for c in tok.text:
                     if c == "\n":
-                        cursor_y += VSTEP * 2
-                        cursor_x = self.width - HSTEP
+                        self.cursor_y += VSTEP * 2
+                        self.cursor_x = self.width - HSTEP
                         continue
                     
-                    self.display_list.append((cursor_x, cursor_y, c, self.font))
-                    cursor_x -= HSTEP
+                    self.display_list.append((self.cursor_x, self.cursor_y, c, self.font))
+                    self.cursor_x -= HSTEP
 
-                    if cursor_x < HSTEP:
-                        cursor_y += VSTEP
-                        cursor_x = self.width - HSTEP
+                    if self.cursor_x < HSTEP:
+                        self.cursor_y += VSTEP
+                        self.cursor_x = self.width - HSTEP
         
         else:
-            cursor_x, cursor_y = HSTEP, VSTEP
+            self.cursor_x = HSTEP
+            self.cursor_y = VSTEP
             for tok in tokens:
-                if isinstance(tok, Text):
-                    for word in tok.text.split():
-                        font = tkf.Font(
-                            size=16,
-                            weight=self.weight,
-                            slant=self.style,
-                        )
-                        w = font.measure(word)
+                self.token(tok)
+    
+    def token(self, tok):
+        if isinstance(tok, Text):
+            for word in tok.text.split():
+                self.word(word)
+        
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
 
-                        if cursor_x + w > self.width - HSTEP - SCROLLBAR_WIDTH:
-                            cursor_y += font.metrics("linespace") * 1.25
-                            cursor_x = HSTEP
-                        
-                        self.display_list.append((cursor_x, cursor_y, word, font))
-                        cursor_x += w + font.measure(" ")
-                
-                elif tok.tag == "i":
-                    self.style = "italic"
-                elif tok.tag == "/i":
-                    self.style = "roman"
-                elif tok.tag == "b":
-                    self.weight = "bold"
-                elif tok.tag == "/b":
-                    self.weight = "normal"
-
+    def word(self, word):
+        font = tkf.Font(
+            size=self.size,
+            weight=self.weight,
+            slant=self.style,
+        )
+        w = font.measure(word)
+        
+        if self.cursor_x + w > self.width - HSTEP - SCROLLBAR_WIDTH:
+            self.cursor_y += font.metrics("linespace") * 1.25
+            self.cursor_x = HSTEP
+        
+        self.display_list.append((self.cursor_x, self.cursor_y, word, font))
+        self.cursor_x += w + font.measure(" ")
